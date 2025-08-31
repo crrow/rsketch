bfb68fd62af811f66f01ade7147c48c655441fc4
crrow <hahadaxigua@gmail.com>
2025-08-31 14:45:40 +0400

    fix buf lint

--- examples/hello-server.rs
+++ examples/hello-server.rs
@@ -24,9 +24,9 @@ fn main() {
         println!("Hello server listening on {}", addr);
 
         tonic::transport::Server::builder()
-            .add_service(rsketch_api::pb::hello::v1::hello_server::HelloServer::new(
-                server,
-            ))
+            .add_service(
+                rsketch_api::pb::hello::v1::hello_service_server::HelloServiceServer::new(server),
+            )
             .serve(addr)
             .await
             .unwrap();
